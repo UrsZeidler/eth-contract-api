61b594d35063b420c5845637f81c1ed77d42db58
UrsZeidler <github@urszeidler.de>
2017-03-04 21:39:43 +0100

    fixing a npe

--- src/main/java/org/adridadou/ethereum/values/EthAddress.java
+++ src/main/java/org/adridadou/ethereum/values/EthAddress.java
@@ -45,7 +45,9 @@ public class EthAddress {
     }
 
     public static EthAddress of(final String address) {
-        if (address != null && address.startsWith("0x")) {
+        if(address==null) 
+        	return empty();
+    	if (address != null && address.startsWith("0x")) {
             return of(Hex.decode(address.substring(2)));
         }
         return of(Hex.decode(address));
